c70c9af23da93ee24e5b3da1415050019182ffe9
GomezUndercover <S1lentD3ath02@gmail.com>
2023-11-14 10:52:33 -0600

    changed comments

--- Arboles/Arbol.java
+++ Arboles/Arbol.java
@@ -1,60 +1,82 @@
+/**
+ * Esta clase nos representa un árbol genérico.
+ *
+ * @param <T> Tipo de datos que almacenará el árbol, puede ser cualquiera pero por razones
+ * de uso practico tenemos la letra T.
+ */
 public class Arbol<T> {
-  private NodoArbol<T> raiz;
-  private boolean esBinario = false;
 
-  // constructor del objeto arbol
-  public Arbol(T dato, boolean esBinario) {
-    raiz = new NodoArbol<T>(dato);
-    raiz.setEsBinario(esBinario);
-    this.esBinario = esBinario;
-  }
+    private NodoArbol<T> raiz;
+    private boolean esBinario = false;
 
-  public NodoArbol<T> obtenerRaiz() {
-    return raiz;
-  }
-
-  public boolean esBinario() {
-    return esBinario;
-  }
-
-  /*
-   * el siguiente metodo toma como parametros dos NodosArbol genericos,
-   * estos nodos se ponen en el orden (nodo raiz, nodo que se agregara)
-   * Al llamar este metodo y darle los parametros correspondientes le agrega
-   * un nodo hijo al nodo raiz que se le indico
-   */
-  public void agregarNodoArbol(NodoArbol<T> nodoRaiz, NodoArbol<T> nodoAgregado) {
-    if (esBinario) {
-      nodoAgregado.setEsBinario(esBinario);
+    /**
+     * Constructor de la clase Arbol.
+     *
+     * @param dato Dato a almacenar en la raíz del árbol.
+     * @param esBinario Nos indica si el árbol es binario o no.
+     */
+    public Arbol(T dato, boolean esBinario) {
+        raiz = new NodoArbol<T>(dato);
+        raiz.setEsBinario(esBinario);
+        this.esBinario = esBinario;
     }
 
-    nodoRaiz.agregarHijo(nodoAgregado);
-  }
-
-  /*
-   * Usando un switch case y tomando como parametro la clase enum Recorrido
-   * se puede imprimir el tipo de recorrido de arbol que se indique
-   * usando los metodos de NodoArbol
-   */
-  public void imprimirArbol(Recorrido recorrido) {
-    switch (recorrido.ordinal()) {
-      case 0:
-        System.out.println("Prefijo: ");
-        raiz.imprimirEnPrefijo();
-        break;
-
-      case 1:
-        System.out.println("\nInfijo: ");
-        raiz.imprimirEnInfijo();
-        break;
-
-      case 2:
-        System.out.println("\nPosfijo: ");
-        raiz.imprimirEnPosfijo();
-        break;
-
-      default:
-        break;
+    /**
+     * Obtiene la raíz del árbol.
+     *
+     * @return NodoArbol que representa la raíz del árbol.
+     */
+    public NodoArbol<T> obtenerRaiz() {
+        return raiz;
     }
-  }
+
+    /**
+     * Verifica si el árbol es binario.
+     *
+     * @return true si el árbol es binario, false sino lo es.
+     */
+    public boolean esBinario() {
+        return esBinario;
+    }
+
+    /**
+     * Agrega un nodo al árbol.
+     *
+     * @param nodoRaiz Nodo padre al que se le agregará el hijo.
+     * @param nodoAgregado Nodo que se agregará como hijo.
+     */
+    public void agregarNodoArbol(NodoArbol<T> nodoRaiz, NodoArbol<T> nodoAgregado) {
+        if (esBinario) {
+            nodoAgregado.setEsBinario(esBinario);
+        }
+
+        nodoRaiz.agregarHijo(nodoAgregado);
+    }
+
+    /**
+     * Imprime el árbol en el recorrido que se indique.
+     *
+     * @param recorrido Tipo de recorrido a utilizar de la clase enum Recorrido.java (Prefijo, Infijo, Posfijo).
+     */
+    public void imprimirArbol(Recorrido recorrido) {
+        switch (recorrido.ordinal()) {
+            case 0:
+                System.out.println("Prefijo: ");
+                raiz.imprimirEnPrefijo();
+                break;
+
+            case 1:
+                System.out.println("\nInfijo: ");
+                raiz.imprimirEnInfijo();
+                break;
+
+            case 2:
+                System.out.println("\nPosfijo: ");
+                raiz.imprimirEnPosfijo();
+                break;
+
+            default:
+                break;
+        }
+    }
 }
