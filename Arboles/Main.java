86133d6fe5993e0ba2850ad9a25441830a2a303c
GomezUndercover <131544924+GomezUndercover@users.noreply.github.com>
2023-11-14 15:03:49 -0600

    Add files via upload

--- Arboles/Main.java
+++ Arboles/Main.java
@@ -20,7 +20,7 @@ public class Main {
         NodoArbol<String> Q = new NodoArbol<String>("Q");
 
 
-        System.out.println("Arbol generico");
+        System.out.println("---ARBOL GENERICO---");
         arbol.agregarNodoArbol(arbol.obtenerRaiz(), B);
         arbol.agregarNodoArbol(arbol.obtenerRaiz(), C);
         arbol.agregarNodoArbol(arbol.obtenerRaiz(), D);
@@ -46,51 +46,67 @@ public class Main {
 
         System.out.println("Impresion en prefijo");
         arbol.imprimirArbol(Recorrido.PREFIJO);
-        System.out.println("Impresion en infijo");
+        System.out.println("\nImpresion en infijo");
         arbol.imprimirArbol(Recorrido.INFIJO);
-        System.out.println("Impresion en posfijo");
+        System.out.println("\nImpresion en posfijo");
         arbol.imprimirArbol(Recorrido.POSFIJO);
+        
+        System.out.println("*******************************************");
 
-        System.out.println("Arbol esta vacio? : " + arbol.estaVacio());
-        System.out.println("Altura de arbol: " + arbol.obtenerAltura());
-
-        System.out.println("Arbol generico representado como binario");
+        System.out.println("---ARBOL BINARIO---");
 
         Arbol<String> arbolBinario = new Arbol<String>(raiz, true);
-        arbolBinario.agregarNodoArbol(arbolBinario.obtenerRaiz(), B);
-        arbolBinario.agregarNodoArbol(B, E);
-        arbolBinario.agregarNodoArbol(B, F);
+        NodoArbol<String> B1 = new NodoArbol<String>("B");
+        NodoArbol<String> C1 = new NodoArbol<String>("C");
+        NodoArbol<String> D1 = new NodoArbol<String>("D");
+        NodoArbol<String> E1 = new NodoArbol<String>("E");
+        NodoArbol<String> F1 = new NodoArbol<String>("F");
+        NodoArbol<String> G1 = new NodoArbol<String>("G");
+        NodoArbol<String> H1 = new NodoArbol<String>("H");
+        NodoArbol<String> I1 = new NodoArbol<String>("I");
+        NodoArbol<String> J1 = new NodoArbol<String>("J");
+        NodoArbol<String> K1 = new NodoArbol<String>("K");
+        NodoArbol<String> L1 = new NodoArbol<String>("L");
+        NodoArbol<String> M1 = new NodoArbol<String>("M");
+        NodoArbol<String> N1 = new NodoArbol<String>("N");
+        NodoArbol<String> O1 = new NodoArbol<String>("O");
+        NodoArbol<String> P1 = new NodoArbol<String>("P");
+        NodoArbol<String> Q1 = new NodoArbol<String>("Q");
+        arbolBinario.agregarNodoArbol(arbolBinario.obtenerRaiz(), B1);
+        arbolBinario.agregarNodoArbol(B1, E1);
+        arbolBinario.agregarNodoArbol(B1, C1);
 
-        arbolBinario.agregarNodoArbol(E, F);
-        arbolBinario.agregarNodoArbol(F, J);
-        arbolBinario.agregarNodoArbol(J, K);
-        arbolBinario.agregarNodoArbol(K, L);
+        arbolBinario.agregarNodoArbol(E1, F1);
+        arbolBinario.agregarNodoArbol(F1, J1);
+        arbolBinario.agregarNodoArbol(J1, K1);
+        arbolBinario.agregarNodoArbol(K1, L1);
 
-        arbolBinario.agregarNodoArbol(C, D);
-        arbolBinario.agregarNodoArbol(D, G);
+        arbolBinario.agregarNodoArbol(C1, D1);
+        arbolBinario.agregarNodoArbol(D1, G1);
 
-        arbolBinario.agregarNodoArbol(G, M);
-        arbolBinario.agregarNodoArbol(G, H);
-        arbolBinario.agregarNodoArbol(M, P);
-        arbolBinario.agregarNodoArbol(P, Q);
+        arbolBinario.agregarNodoArbol(G1, M1);
+        arbolBinario.agregarNodoArbol(G1, H1);
+        arbolBinario.agregarNodoArbol(M1, P1);
+        arbolBinario.agregarNodoArbol(P1, Q1);
 
-        arbolBinario.agregarNodoArbol(H, I);
-        arbolBinario.agregarNodoArbol(I, N);
-        arbolBinario.agregarNodoArbol(N, O);
+        arbolBinario.agregarNodoArbol(H1, I1);
+        arbolBinario.agregarNodoArbol(I1, N1);
+        arbolBinario.agregarNodoArbol(N1, O1);
 
         System.out.println("Impresion en prefijo");
         arbolBinario.imprimirArbol(Recorrido.PREFIJO);
-        System.out.println("Impresion en infijo");
+        System.out.println("\nImpresion en infijo");
         arbolBinario.imprimirArbol(Recorrido.INFIJO);
-        System.out.println("Impresion en posfijo");
+        System.out.println("\nImpresion en posfijo");
         arbolBinario.imprimirArbol(Recorrido.POSFIJO);
 
-        System.out.println("Arbol esta vacio? : " + arbolBinario.estaVacio());
-        System.out.println("Altura de arbol: " + arbolBinario.obtenerAltura());
+        System.out.println("\nArbol esta vacio? : " + arbolBinario.estaVacio());
+        System.out.println("\nAltura de arbol: " + arbolBinario.obtenerAltura());
 
+        System.out.println("------------------------------------------------");
 
         System.out.println("Impresion de subarbol a partir del nodo D del arbol binario");
-        arbolBinario.imprimirSubArbol(D);
+        arbolBinario.imprimirSubArbol(D1);
 
 
         
